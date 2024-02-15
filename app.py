9ddcace03c1f1038cff3d1e236ef57823b9fd7bb
HemachandranD <hema18deena@gmail.com>
2024-02-15 19:00:15 +0530

    updated the openai api and added requirements

--- app.py
+++ app.py
@@ -19,13 +19,15 @@ hide_default_format = """
 st.markdown(hide_default_format, unsafe_allow_html=True)
 left, right = st.columns(2)
 
+
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown("<style>{}</style>".format(f.read()), unsafe_allow_html=True)
 
+
 def setup():
-    transcript=''
-    custom_prompt= ''
+    transcript = ""
+    custom_prompt = ""
 
     with left:
         html_temp = """
@@ -34,76 +36,101 @@ def setup():
         """
         st.markdown(html_temp, unsafe_allow_html=True)
         st.write(""" """)  # description and instructions
-        audio = st.file_uploader("Upload the audio to be analyzed and ask Questions\n", type=["mp3", "wav"], label_visibility="visible")
+        audio = st.file_uploader(
+            "Upload the audio to be analyzed and ask Questions\n",
+            type=["mp3", "wav"],
+            label_visibility="visible",
+        )
 
         if audio is None:
             st.text("")
         else:
             with left:
-                if 'transcript' not in st.session_state:
-                    st.session_state['transcript'] = get_transcribe_audio(audio)
-                transcript = st.session_state['transcript']
+                if "transcript" not in st.session_state:
+                    st.session_state["transcript"] = get_transcribe_audio(audio)
+                transcript = st.session_state["transcript"]
                 st.subheader("Play Audio")
                 st.audio(audio)
 
         with st.form("Launch_form"):
-                task=st.radio('What would you like me to do?', options=['Transcribe :speech_balloon:', 'Summary :memo:', 'Highlights :chart_with_upwards_trend:', ':rainbow[Custom] :rocket:'], horizontal=False, \
-                        captions=["Get the transcription of the complete Audio. ", "Get the Summary of the Transcribed Audio.",\
-                                "Get the Highlights of the Audio.", "Ask questions about the Audio and get the Answers from Powerful GPT."])
+            task = st.radio(
+                "What would you like me to do?",
+                options=[
+                    "Transcribe :speech_balloon:",
+                    "Summary :memo:",
+                    "Highlights :chart_with_upwards_trend:",
+                    ":rainbow[Custom] :rocket:",
+                ],
+                horizontal=False,
+                captions=[
+                    "Get the transcription of the complete Audio. ",
+                    "Get the Summary of the Transcribed Audio.",
+                    "Get the Highlights of the Audio.",
+                    "Ask questions about the Audio and get the Answers from Powerful GPT.",
+                ],
+            )
 
-                custom_prompt = st.text_input("Enter the message")
+            custom_prompt = st.text_input("Enter the message")
 
-                # Every form must have a submit button.
-                submitted = st.form_submit_button("Launch", type='primary')
+            # Every form must have a submit button.
+            submitted = st.form_submit_button("Launch", type="primary")
 
         return submitted, task, transcript, custom_prompt
+
 
 def launch_task(task, transcript, custom_prompt):
     message = st.chat_message("assistant")
 
     if task == "Transcribe :speech_balloon:":
-                message.write("Transcription")
-                message.write(transcript)
+        message.write("Transcription")
+        message.write(transcript)
 
-    elif task == 'Summary :memo:':
+    elif task == "Summary :memo:":
         with st.spinner("Summarizing the content..."):
-                instruction_prompt="Summarize the following"
-                request= instruction_prompt + transcript
-                if 'summary' not in st.session_state:
-                    st.session_state['summary'] = get_custom_response(request=request)
-                summary=st.session_state['summary']
+            instruction_prompt = "Summarize the following"
+            request = instruction_prompt + transcript
+            if "summary" not in st.session_state:
+                st.session_state["summary"] = get_custom_response(request=request)
+            summary = st.session_state["summary"]
         message.write("Summary")
         message.write(summary)
 
-    elif task == 'Highlights :chart_with_upwards_trend:':
+    elif task == "Highlights :chart_with_upwards_trend:":
         with st.spinner("Picking up the Highlights..."):
-            highlights_request= "Extract the Highlights from the following" + transcript
-            if 'highlights' not in st.session_state:
-                    st.session_state['highlights'] = get_custom_response(request=highlights_request)
-            highlights=st.session_state['highlights']
+            highlights_request = (
+                "Extract the Highlights from the following" + transcript
+            )
+            if "highlights" not in st.session_state:
+                st.session_state["highlights"] = get_custom_response(
+                    request=highlights_request
+                )
+            highlights = st.session_state["highlights"]
         message.write("Audio Highlights")
         message.write(highlights)
 
-    elif task == ':rainbow[Custom] :rocket:' and custom_prompt!='':
+    elif task == ":rainbow[Custom] :rocket:" and custom_prompt != "":
         with st.spinner("Generating the Response..."):
-            oofContext_prompt= "Answer only from the attached text"
-            custom_request= oofContext_prompt + ' ' +custom_prompt +' '+ transcript[:100]
+            oofContext_prompt = "Answer only from the attached text"
+            custom_request = (
+                oofContext_prompt + " " + custom_prompt + " " + transcript[:100]
+            )
             answer = get_custom_response(request=custom_request)
         message.write("Hello Human")
         message.write(answer)
+
 
 def main():
     load_css("src/style.css")
     submitted, task, transcript, custom_prompt = setup()
     with right:
-            if submitted and transcript != '':
-                st.toast(f'Your task for {task} is submitted', icon='🚀')
-                time.sleep(.5)
-                launch_task(task, transcript, custom_prompt)
-            else:
-                 with left:
-                    st.write("")
+        if submitted and transcript != "":
+            st.toast(f"Your task for {task} is submitted", icon="🚀")
+            time.sleep(0.5)
+            launch_task(task, transcript, custom_prompt)
+        else:
+            with left:
+                st.write("")
 
 
 if __name__ == "__main__":
-        main()
+    main()
--- src/askaudio.py
+++ src/askaudio.py
@@ -1,32 +1,32 @@
 import streamlit as st
 import time
 from openai import OpenAI
+import os
 
 client = OpenAI()
 
 
 def get_transcribe_audio(audio):
-        with st.spinner("Patience is the key to accurate analysis. Please wait while we process your audio"):
-            transcript = client.audio.transcribe(
-                file = audio,
-                model = "whisper-1",
-                response_format="text",
-                language="en"
-            )
-        with st.spinner("Hold the line, we're diving deep into your audio."):
-              time.sleep(1)
-        st.toast('Your Audio File is analyzed', icon='😍')
-        time.sleep(.5)
-        return transcript
+    with st.spinner(
+        "Patience is the key to accurate analysis. Please wait while we process your audio"
+    ):
+        transcript = client.audio.transcriptions.create(
+            file=audio, model="whisper-1", response_format="text", language="en"
+        )
+    with st.spinner("Hold the line, we're diving deep into your audio."):
+        time.sleep(1)
+    st.toast("Your Audio File is analyzed", icon="😍")
+    time.sleep(0.5)
+    return transcript
+
 
 def get_custom_response(request):
-      chatOutput= openai.ChatCompletion.create(model="gpt-3.5-turbo-16k",messages=[{
-        "role": "system",
-        "content": "You are a helpful assistant."
-      },
-      {
-        "role": "user",
-        "content": request
-      }])
-      customResponse = chatOutput.choices[0].message.content
-      return customResponse
+    chatOutput = client.chat.completions.create(
+        model="gpt-3.5-turbo-16k",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": request},
+        ],
+    )
+    customResponse = chatOutput.choices[0].message.content
+    return customResponse
